29f46f2df4306a8ea329fd66b8d6df7d977d99ba
Knopf <ac1ifci@aidemucmc007451.fritz.box>
2026-02-04 11:25:47 +0100

    feat(localdraw): fix asset urls

---
+++ localdraw/dist/assets/channel-Dv-acGfw.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"./index-C6uo3prc.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- localdraw/vite.config.js
+++ localdraw/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/localdraw/dist/',
+  base: './',
   plugins: [react()],
   define: {
     'process.env.IS_PREACT': JSON.stringify('false'),
