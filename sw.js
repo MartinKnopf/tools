f21f530d8e8542eb04d080521de98aacb742190b
Knopf <ac1ifci@aidemucmc007451.fritz.box>
2026-02-10 22:07:08 +0100

    harmoize styling

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 // Service Worker for Static Tools PWA
-const CACHE_NAME = 'static-tools-v10';
+const CACHE_NAME = 'static-tools-v11';
 
 // Files to cache on install
 const PRECACHE_URLS = [
   '/tools/',
   '/tools/index.html',
   '/tools/manifest.json',
+  '/tools/style.css',
   '/tools/spidey/index.html',
   '/tools/rank-it/index.html',
   '/tools/sleeptimer/index.html',
